43d83c0f2740796d96b07fb3b2fdb70aeb7adf56
charlie <jhdhj999@gmail.com>
2025-11-05 13:34:51 +0600

    Validation added to the put request

--- main.py
+++ main.py
@@ -43,7 +43,10 @@ async def create_task(db: db_dependency, tocrush_request: TocrushRequest):
     db.commit()
     
 @app.put("/tocrush/{tocrush_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_tocrush(db: db_dependency, tocrush_id: int, tocrush_request: TocrushRequest):
+async def update_tocrush(db: db_dependency, 
+                         tocrush_request: TocrushRequest,
+                         tocrush_id: int = Path(gt=0),                          
+                        ):
     tocrush_model = db.query(Tocrush).filter(Tocrush.id == tocrush_id).first()
     if tocrush_model is None:
         raise HTTPException(satus_code=404, detail="Task not found!")
