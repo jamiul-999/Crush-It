2af5cf6381f254d94e56225deeb911cbdbec84ce
charlie <jhdhj999@gmail.com>
2025-11-05 13:01:44 +0600

    Get by ID request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 import models
 from models import Tocrush
 from database import engine, SessionLocal
@@ -21,3 +21,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Tocrush).all()
+
+@app.get("/tocrush/{tocrush_id}", status_code=status.HTTP_200_OK)
+async def read_tocrush(db: db_dependency, tocrush_id: int = Path(gt=0)):
+    tocrush_model = db.query(Tocrush).filter(Tocrush.id == tocrush_id).first()
+    if tocrush_model is not None:
+        return tocrush_model
+    raise HTTPException(status_code=404, detail="Task not found!")
