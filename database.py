1eacf506cf770b6dc512b1add001dff3f5972aa4
charlie <jhdhj999@gmail.com>
2025-11-15 15:09:29 +0600

    Fix typo in variable name for PostgreSQL database URL

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-POSGRESQL_DATABASE_URL = 'postgresql://postgres:jk#*943p2k@localhost/TocrushApplicationDatabase'
+POSTGRESQL_DATABASE_URL = 'postgresql://postgres:jk#*943p2k@localhost/TocrushApplicationDatabase'
 
-engine = create_engine(POSGRESQL_DATABASE_URL)
+engine = create_engine(POSTGRESQL_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
