60ec5139f6bb05edb9db6e88f08c88e51989c2c5
charlie <jhdhj999@gmail.com>
2025-11-15 13:55:34 +0600

    Add password change functionality to user router

--- routers/users.py
+++ routers/users.py
@@ -23,8 +23,26 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+class UserVerfication(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return db.query(Users).filter(Users.id == user.get('id')).first()
+
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency,
+                          db: db_dependency,
+                          user_verification: UserVerfication):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
