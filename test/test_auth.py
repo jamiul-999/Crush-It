0f23a85b41bcd72748583828a7aba37e3da0ec5c
charlie <jhdhj999@gmail.com>
2025-11-18 01:56:18 +0600

    Add tests for access token creation and enhance user authentication tests

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,5 +1,8 @@
 from .utils import *
-from routers.auth import get_db, authenticate_user
+from routers.auth import get_db, authenticate_user, create_access_token
+from config import SECRET_KEY, ALGORITHM
+from jose import jwt
+from datetime import timedelta
 
 app.dependency_overrides[get_db] = override_get_db
 
@@ -16,3 +19,17 @@ def test_authenticate_user(test_user):
     
     wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
     assert wrong_password_user is False or non_existent_user is None
+    
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+    
+    token = create_access_token(username, user_id, role, expires_delta)
+    decoded_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM],
+                               options={'verify_signature': False})
+    
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
