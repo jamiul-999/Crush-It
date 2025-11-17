1504d95564570f9209b77dd439ad626267fd20c0
charlie <jhdhj999@gmail.com>
2025-11-18 01:11:51 +0600

    Add test for deleting tocrush item and ensure proper response

--- test/test_admin.py
+++ test/test_admin.py
@@ -1,6 +1,7 @@
 from .utils import *
 from routers.admin import get_db, get_current_user
 from fastapi import status
+from models import Tocrush
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
@@ -11,3 +12,11 @@ def test_admin_read_all_authenticated(test_tocrush):
     assert response.json() == [{'complete': False, 'title': 'Solve leetcode',
                                 'description': 'Do it everyday!', 'id': test_tocrush.id,
                                 'priority': 5, 'owner_id': test_tocrush.owner_id}]
+    
+def test_admin_delete_tocrush(test_tocrush):
+    response = client.delete(f'/admin/tocrush/{test_tocrush.id}')
+    assert response.status_code == 204
+    
+    db = TestingSessionLocal()
+    model = db.query(Tocrush).filter(Tocrush.id == test_tocrush.id).first()
+    assert model is None
