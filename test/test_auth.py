3555d0d5de541702b526daa6d713b05af0ab4ebd
charlie <jhdhj999@gmail.com>
2025-11-18 01:45:32 +0600

    Add initial test for user authentication functionality

---
+++ test/test_auth.py
@@ -0,0 +1,10 @@
+from .utils import *
+from routers.auth import get_db, authenticate_user
+
+app.dependency_overrides[get_db] = override_get_db
+
+def test_authenticate_user(test_user):
+    db = TestingSessionLocal()
+    authenticated_user = authenticate_user(test_user.username, 'fakehashedpassword', db)
+    assert authenticated_user is not None
+    assert authenticated_user.username == test_user.username
