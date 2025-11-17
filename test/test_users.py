144c3de59dd508da5262fe46245497b1943fb097
charlie <jhdhj999@gmail.com>
2025-11-18 01:31:20 +0600

    Add test for returning user details with expected response

---
+++ test/test_users.py
@@ -0,0 +1,17 @@
+from .utils import *
+from routers.users import get_db, get_current_user
+from fastapi import status
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+def test_return_user(test_user):
+    response = client.get("/user")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() ['username'] == 'jhdtest'
+    assert response.json()['email'] == 'test@example.com'
+    assert response.json()['first_name'] == 'Test'
+    assert response.json()['last_name'] == 'User'
+    assert response.json()['is_active'] == True
+    assert response.json()['role'] == 'admin'
+    
