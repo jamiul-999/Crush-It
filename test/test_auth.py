6db4965f7660ae954a445499a31d5d6221b90d6a
charlie <jhdhj999@gmail.com>
2025-11-18 01:51:29 +0600

    Refactor test for user authentication to improve assertions and clarity

--- test/test_auth.py
+++ test/test_auth.py
@@ -5,6 +5,14 @@ app.dependency_overrides[get_db] = override_get_db
 
 def test_authenticate_user(test_user):
     db = TestingSessionLocal()
+    
     authenticated_user = authenticate_user(test_user.username, 'fakehashedpassword', db)
-    assert authenticated_user is not None
+    assert authenticated_user is not None or authenticated_user is not False
+    #assert authenticate_user is not False
     assert authenticated_user.username == test_user.username
+    
+    non_existent_user = authenticate_user('WrongUserName', 'fakehashedpassword', db)
+    assert non_existent_user is False or non_existent_user is None
+    
+    wrong_password_user = authenticate_user(test_user.username, 'wrongpassword', db)
+    assert wrong_password_user is False or non_existent_user is None
